058c2ce0ecc04354d53c1391098b830a0173c0b2
c-yi <2446396891@qq.com>
2019-06-11 11:21:33 +0800

    '新增git问题'

--- config/sidebar/tools/index.js
+++ config/sidebar/tools/index.js
@@ -1,5 +1,5 @@
 const utils = require('../../../utils');
 
-let git = utils.genSidebar('Git', ['']);
+let git = utils.genSidebar('Git', ['', '遇到的坑']);
 let webpack = utils.genSidebar('webpack', ['', '优化', '常见loader']);
 module.exports = {git, webpack};
