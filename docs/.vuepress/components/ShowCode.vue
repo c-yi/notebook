4b717078b4778ee8b7ce478367aade6d0d35dbb2
c-yi <2446396891@qq.com>
2020-01-15 23:42:31 +0800

    添加代码隐藏 - 2020/01/15 23:42:02

---
+++ docs/.vuepress/components/ShowCode.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <div class="show-button" @click="codeShow=!codeShow">
+            {{codeShow?'隐藏代码 △ ':'显示代码 ▽ '}}
+        </div>
+        <div v-show="codeShow">
+            <slot></slot>
+        </div>
+        <div class="show-button" @click="codeShow=!codeShow" v-show="codeShow">
+            {{codeShow?'隐藏代码 △ ':'显示代码 ▽ '}}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ShowCode',
+        data() {
+            return {
+                codeShow: false
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .show-button {
+        width: 100%;
+        height: 40px;
+        color: orangered;
+        font-weight: 700;
+        background-color: #EEE;
+        text-align: center;
+        line-height: 40px;
+        cursor: pointer;
+    }
+
+</style>
--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,15 +1,3 @@
 /**
  * 擴展 VuePress 應用
  */
-
-//import './public/css/index.css' //組件css文件
-import codeRain from './components/funny/codeRain'
-
-export default ({
-                    Vue, // VuePress 正在使用的 Vue 構造函數
-                    options, // 附加到根實例的一些選項
-                    router, // 當前應用的路由實例
-                    siteData // 站點元數據
-                }) => {
-    Vue.component(codeRain)
-}
